79914433734f00bc4e980724e5541f7fa4884a01
Sharath Dharmala <sharath.dharmala@gmail.com>
2025-12-25 18:03:17 +0530

    fixes of footermail and sliders

--- src/app/data/real-estate.config.ts
+++ src/app/data/real-estate.config.ts
@@ -69,6 +69,33 @@ export const REAL_ESTATE_CONFIG: Record<string, AreaConfig[]> = {
         /*[{ basePath: 'banners/farm/anakapalli', count: 2 }]*/
       ),
     },
+    {
+      key: 'devarapalli',
+      name: 'Devarapalli Chandan Valley',
+      description: '**10-Year-Old Red & White Sandalwood',
+      images: generateImages(
+        ['banners/farm-lands.jpg'],
+        [{ basePath: 'farm-lands/devarapalli-chandanvalley', count: 6 }]
+      ),
+    },
+    {
+      key: 'devarapalli',
+      name: 'Devarapalli Phenix Valley',
+      description: '**Karjur, Agarwood',
+      images: generateImages(
+        ['banners/farm-lands.jpg'],
+        [{ basePath: 'farm-lands/devarapalli-phenixvalley', count: 2 }]
+      ),
+    },
+    {
+      key: 'gajapathinagaram',
+      name: 'Gajapathinagaram',
+      description: '**White Sandle',
+      images: generateImages(
+        ['banners/farm-lands.jpg'],
+        [{ basePath: 'farm-lands/gajapathinagaram', count: 2 }]
+      ),
+    },
   ],
 
   /* ===============================
@@ -90,13 +117,13 @@ export const REAL_ESTATE_CONFIG: Record<string, AreaConfig[]> = {
       key: 'tagarapuvalasa',
       name: 'Tagarapuvalasa,TrueCapital ',
       images: generateImages(['banners/open-plots.jpg'],
-      [{ basePath: 'open-plots/tagarapuvalasa-bbg', count: 4 }])
+      [{ basePath: 'open-plots/tagarapuvalasa-bbg-truecapital', count: 8 }])
     },
     {
       key: 'gandigundam',
       name: 'Gandigundam',
       images: generateImages(['banners/open-plots.jpg'],
-      [{ basePath: 'open-plots/tagarapuvalasa-bbg', count: 6 }]),
+      [{ basePath: 'open-plots/gandigundam', count: 6 }]),
     },
     {
       key: 'vizianagaram',
@@ -131,11 +158,12 @@ export const REAL_ESTATE_CONFIG: Record<string, AreaConfig[]> = {
     },
     {
       key: 'kothavalasa',
-      name: 'Kothavalasa',
-      images: generateImages(['banners/open-plots.jpg']),
+      name: 'Kothavalasa-',
+      images: generateImages(['banners/open-plots.jpg'],
+        [{ basePath: 'open-plots/kothavalasa', count: 3 }]
+      ),
     },
   ],
-
   /* ===============================
      RESIDENTIAL FLATS
   ============================== */
