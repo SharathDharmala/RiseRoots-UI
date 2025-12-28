c4db3c535860c002f262a7012482c62a969d7fc8
Sharath Dharmala <sharath.dharmala@gmail.com>
2025-12-28 08:38:51 +0530

    Ignore all video files

--- src/app/data/media.config.model.ts
+++
@@ -1,5 +0,0 @@
-export interface MediaSourceConfig {
-  basePath: string;   
-  count: number;      
-  ext?: 'jpg' | 'jpeg' | 'png' | 'webp' | 'mp4' | 'webm' | 'JPG' | 'JPEG' | 'PNG' | 'WEBP' | 'MP4' | 'WEBM';
-}
